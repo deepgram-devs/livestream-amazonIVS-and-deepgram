4c487db8a874c4771b4216b800d183e837fb4cb4
Sandra Rodgers <sandra.rodgers@deepgram.com>
2022-03-15 16:08:22 -0500

    added simple Vuex no backend

--- src/views/EnterCode.vue
+++ src/views/EnterCode.vue
@@ -1,14 +1,6 @@
 <template>
   <div class="enter-code">
-    <h1>VIP Live Stream</h1>
-    <p>
-      A live stream you have been specially invited to.... powered by Deepgram,
-      Amazon IVS, and OBS.
-    </p>
-    <img src="../assets/dg-logo.svg" class="logo" />
-    <p>Deepgram Demos</p>
     <h2 class="form-title">Enter with the code:</h2>
-
     <form @submit.prevent="submitCode">
       <label for="code"> Code: </label>
       <input v-model="code" type="password" name="code" value />
@@ -31,28 +23,17 @@ export default {
     //Verify Code
     let code = ref("");
     function submitCode() {
-      const requestOptions = {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ code: code }),
-      };
-      fetch("http://localhost:8080/secret-code", requestOptions)
-        .then((response) => response.json())
-        .then((data) => {
-          if (data === "Correct code") {
-            store.dispatch("verifyCode", true);
-          } else {
-            store.dispatch("verifyCode", false);
-          }
-        })
-        .then(() => {
-          if (store.state.allowAccess) {
-            router.push({ name: "StreamChannel" });
-          } else {
-            alert("Incorrect code");
-          }
-        });
-
+      if (code.value === "pb") {
+        store.dispatch("verifyCode", true);
+      } else {
+        store.dispatch("verifyCode", false);
+      }
+      // Navigate if correct code, alert if not correct
+      if (store.state.allowAccess) {
+        router.push({ name: "StreamChannel" });
+      } else {
+        alert("Incorrect code");
+      }
       code.value = "";
     }
 
@@ -60,28 +41,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-form {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.form-title {
-  margin-top: 100px;
-}
-
-.logo {
-  height: 10vh;
-  margin-top: 100px;
-}
-
-.enter-code > h1 {
-  font-size: 3.5em;
-}
-
-.enter-code > p {
-  font-size: 1.2em;
-}
-</style>
